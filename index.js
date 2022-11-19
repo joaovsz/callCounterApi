d277ea219d9a06a176225ffc6dbe7f104cff4d75
joaovsz <joaovitor1713coin@gmail.com>
2022-11-19 20:47:17 -0300

    port modified

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const userRouter = require("./routes/userRouter");
 app.use(express.json());
 app.use(cors());
 app.use("/user", userRouter);
-const port = 3040;
+const port = 3000;
 app.listen(port, (err, res) => {
   console.log("Rodando", port);
 });
